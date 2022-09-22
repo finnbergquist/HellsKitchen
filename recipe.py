5d9cdd8e762ec5f51aba5f0330f9de6513d9bd17
finnbergquist <fbergqui@bowdoin.edu>
2022-09-22 08:57:16 -0400

    Update recipe.py

--- recipe.py
+++ recipe.py
@@ -3,34 +3,45 @@ from ingredient import Ingredient
 
 class Recipe:
     """
-    Recipe represents a single recipe, with a dictionary that stores the ingredients.
-
-    key value pair ex:  ingredients['chicken'] = 10
+    Recipe represents a single recipe, with a list that stores its ingredients. It also must contain
+    a list of all the possible ingredient names(inspiring_ingredients), so that it can choose new ingredients
+    for mutation type 2 and 3. Finally, it also has a name to label the recipe.
     """
 
 
-    def __init__(self, ingredients, name, inspiring_ingredients):
-        """
-        Constructor for the recipe class.
+    def __init__(self, ingredients, name, inspiring_ingredients, mutation_probability=0.2):
+        """Constructor for the recipe class.
         Args:
-            ingredients (dict - might change to list(Ingredient)): the list of all ingredients and amounts for the recipe
-                        ex.
-                            rice : 0.4
-                            beans : 0.6
-
+            ingredients (list) : list of ingredient objects
             name (str): the name of the recipe
+            inspiring_ingredients (set) : original set of all ingredients that can be used in mutation
+        Return:
+            None
         """
         self.inspiring_ingredients = inspiring_ingredients# set of ingredient_names
         self.ingredients = ingredients # list of ingredient objects
         self.name = name
+        self.mutation_probability = mutation_probability
 
-    def fitness(self):
+
+    def get_fitness(self):
+        '''Fitness metric as specified in the requirements.
+        Args: 
+            None
+        Return:
+            number of ingredients (int)
+        '''
+
         return len(self.ingredients)
 
     def normalize(self):
-        '''
-        Sums the total amount of ingredients(in ounces). Then it multiplies each ingredient amount by
-        100/total volume so that the sum of all ingredients sums to 100 again.
+        ''' Sums the total amount of ingredients(in ounces). Then it multiplies each ingredient amount by
+        100/total volume so that the sum of all ingredients sums to 100 again. The self.ingredients list
+        is modified.
+        Args: 
+            None
+        Return:
+            None
         '''
 
         total_amount = 0.
@@ -45,8 +56,9 @@ class Recipe:
     def available_ingredients(self):
         '''
         Looks at all the inspiring ingredients, and chooses an ingredient from that list which is not already
-        in the recipe.
-
+        in the recipe. The edge case that there are no available ingredients is dealt with in the mutate method.
+        Args:
+            None
         Return:
             available_ingredients(list) : list of ingredient objects
         '''
@@ -55,7 +67,7 @@ class Recipe:
         for ingredient in self.ingredients:
             used_ingredient_names.add(ingredient.name)
 
-        available_ingredient_names = set(self.inspiring_ingredients) - used_ingredient_names
+        available_ingredient_names = self.inspiring_ingredients - used_ingredient_names
 
         return list(available_ingredient_names)
 
@@ -63,18 +75,17 @@ class Recipe:
     def mutate(self):
         """
         Based on random probability, choose whether or not we will mutate
-
             If we are gonna mutate, choose between 4 different mutation possibilities:
-
             1: randomly pick an ingredient in its recipe, randomly change its amount, renormalize
-
             2: change one ingredient to another: use the same ammount, renomrmalize
-
             3: addition of an ingredient, renormalize
-
             4: deletion of an ingredient, renormalize
+        Args:
+            None
+        Return:
+            None
         """
-        mutation_probability = 0.2
+        mutation_probability = self.mutation_probability
 
         #Step 1: Decide if we will mutate
         if random.random() > mutation_probability: 
@@ -116,8 +127,10 @@ class Recipe:
     def getIngredients(self):
         return self.ingredients
 
-    def get_fitness(self):
-        return len(self.ingredients)
-
     def __str__(self):
+        '''
+       using __str__ method of ingredients, and joining them all together 
+        Return:
+            Name (str) concatenated string of all ingredient names and ammounts
+        '''
         return self.name + ": " + ", ".join([str(ingredient) for ingredient in self.ingredients])
